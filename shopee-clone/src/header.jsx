5abf15c035b9084c62835e6147ac1314d0c3241c
John Kyle Joseph Reyes <jkj313.kr@gmail.com>
2023-06-15 20:46:00 +0800

    adjustments

--- shopee-clone/src/header.jsx
+++ shopee-clone/src/header.jsx
@@ -24,7 +24,7 @@ function Header() {
             >
               Search
             </label>
-            <div class="relative">
+            <div class="relative ml-96">
               <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg
                   aria-hidden="true"
@@ -45,7 +45,7 @@ function Header() {
               <input
                 type="search"
                 id="default-search"
-                class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search Products, Services..."
                 required
               ></input>
@@ -58,27 +58,27 @@ function Header() {
             </div>
           </form>
         </Navbar.Brand>
-        <div className="flex items-center ml-4 rounded-lg p-2">
-          <p className="leading-none pr-7">
+        <div className="flex items-center ml-96 rounded-lg p-2">
+          <p className="leading-none pr-2">
             <button class="text-white bg-gradient-to-br from-orange-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
               <span className="navtitle-md font-semibold text-black">
                 Notifications
               </span>
             </button>
           </p>
-          <p className="leading-none pr-7">
+          <p className="leading-none pr-2">
             <button class="text-white bg-gradient-to-br from-orange-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
               <span className="navtitle-md font-semibold text-black">Cart</span>
             </button>
           </p>
-          <p className="leading-none pr-7">
+          <p className="leading-none pr-2">
             <button class="text-white bg-gradient-to-br from-orange-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
               <span className="navtitle-md font-semibold text-black">
                 Sign up
               </span>
             </button>
           </p>
-          <p className="leading-none pr-7">
+          <p className="leading-none pr-2">
             <button class="text-white bg-gradient-to-br  from-orange-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
               <span className="navtitle-md font-semibold text-black">
                 Login
