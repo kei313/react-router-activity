2d4dbc1412cc7b774b3c4338fefd27ad1d3971f1
John Kyle Joseph Reyes <jkj313.kr@gmail.com>
2023-06-19 10:26:54 +0800

    added banner

--- shopee-clone/src/App.jsx
+++ shopee-clone/src/App.jsx
@@ -2,21 +2,31 @@ import { Carousel } from "flowbite-react";
 import "./index.css";
 import Timer from "./timer.jsx";
 
-const products = ["https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcS5o_Lgj-AlSKdKld0LJWZLlAAFNriw4KvRea3vS3nUA2JMgR-CZRqGqmVGJODeHhxobyEWJjbP0mRKIbEWjhwiDrKxBjFWcjfUpRav-6at&usqp=CAE","https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRoc18szOBd9Xhh4NPg0ALLuDw2wCG6PfYFkUIM9xJSGkniDWSG7L2LmN0L5qMMJ6U8aXpNOXw6fg0D2cUeGeSZVJNqytpTUWoFIZzVnBneZV_e61cG23Ne4Q&usqp=CAE","https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSIM7W2HCvSMcsdN2ad4E2CswpQEjvsSDl0KzyHSIWSF2pE9wQ3fcwAnPRyebqlqLbuAgYelaQl_bU4J88wxFq26bFGioopCV2LoM8Jc6E&usqp=CAE","https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQmJB5ll15UH2xnqzFf4WTp6wQN4S5vqfwSLZrOd4V7frEvO_qrgK3v6STFnHgG2Xmq1roU-HG6bnagpjaaflFMAv9r61qulRyejqZjWIbiXUiMLRKBQxz8LA&usqp=CAE","https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQBN4nDsD2LuimHeBdO87pPVqN7wnPikGMhORIttJA65Mr5aEg7EWB7YgOcgzh7Z3aUeBJ0wnQhaoRUazCfioo0QucVf4UbwcfKiBDUuXig&usqp=CAE","https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS292hXuxh0XHDpuKHEBxix7R_GdvYSiFZgAdDJwmBvKe5ttvk4dKATXE5C5GrtDEDuIlV1ULRK476ODigAFzhYmCEvsvvj97foJ9RFCdW7&usqp=CAE","https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSLaKJpbClA14_E-OZTJW5JrYOiIp9c0xxrrqCk_FuQPZfIsN6WwZPvr3HopvXJYBedUZAOcejTAin0KKUv018JvBxFy0JraDrBPi3FwxFRfwxiX5YVfXL6Pw&usqp=CAE","https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTBfjXcfyYYG0_OzsnhL85yK9OCW9Snygb5VrbZG228p51S5pbnMOZOXHMWLE79BqNsPGt5ifzGcTNtypdw8no-ZOUrj1j29vpY5J8RL2VD87tEcxilR98A&usqp=CAE","https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSOoOH5mdA-y525_pjF8qf2_0lR81n2VJGZTvAYrrTxPGMNfY9-h0LDEaGfqpqVZgn2ywVelcj0UJHi765A5r9J7mhbkb9--xccFul7d6uUorrO8NtdyGqw&usqp=CAE","https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQEXEUeU-28HIqlLELYZOLfAJwTKd_4sKIqIv6zTT-A5nORzFaYVV2zB8VBB_314mUa_93JkP0qiRAs1pC_GniNIA3EWCOmTqI_CWWtnqQ&usqp=CAE"];
-const hoursMinSecs = {hours:2, minutes: 20, seconds: 40}
+const products = [
+  "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcS5o_Lgj-AlSKdKld0LJWZLlAAFNriw4KvRea3vS3nUA2JMgR-CZRqGqmVGJODeHhxobyEWJjbP0mRKIbEWjhwiDrKxBjFWcjfUpRav-6at&usqp=CAE",
+  "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRoc18szOBd9Xhh4NPg0ALLuDw2wCG6PfYFkUIM9xJSGkniDWSG7L2LmN0L5qMMJ6U8aXpNOXw6fg0D2cUeGeSZVJNqytpTUWoFIZzVnBneZV_e61cG23Ne4Q&usqp=CAE",
+  "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSIM7W2HCvSMcsdN2ad4E2CswpQEjvsSDl0KzyHSIWSF2pE9wQ3fcwAnPRyebqlqLbuAgYelaQl_bU4J88wxFq26bFGioopCV2LoM8Jc6E&usqp=CAE",
+  "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQmJB5ll15UH2xnqzFf4WTp6wQN4S5vqfwSLZrOd4V7frEvO_qrgK3v6STFnHgG2Xmq1roU-HG6bnagpjaaflFMAv9r61qulRyejqZjWIbiXUiMLRKBQxz8LA&usqp=CAE",
+  "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQBN4nDsD2LuimHeBdO87pPVqN7wnPikGMhORIttJA65Mr5aEg7EWB7YgOcgzh7Z3aUeBJ0wnQhaoRUazCfioo0QucVf4UbwcfKiBDUuXig&usqp=CAE",
+  "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS292hXuxh0XHDpuKHEBxix7R_GdvYSiFZgAdDJwmBvKe5ttvk4dKATXE5C5GrtDEDuIlV1ULRK476ODigAFzhYmCEvsvvj97foJ9RFCdW7&usqp=CAE",
+  "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSLaKJpbClA14_E-OZTJW5JrYOiIp9c0xxrrqCk_FuQPZfIsN6WwZPvr3HopvXJYBedUZAOcejTAin0KKUv018JvBxFy0JraDrBPi3FwxFRfwxiX5YVfXL6Pw&usqp=CAE",
+  "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTBfjXcfyYYG0_OzsnhL85yK9OCW9Snygb5VrbZG228p51S5pbnMOZOXHMWLE79BqNsPGt5ifzGcTNtypdw8no-ZOUrj1j29vpY5J8RL2VD87tEcxilR98A&usqp=CAE",
+  "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSOoOH5mdA-y525_pjF8qf2_0lR81n2VJGZTvAYrrTxPGMNfY9-h0LDEaGfqpqVZgn2ywVelcj0UJHi765A5r9J7mhbkb9--xccFul7d6uUorrO8NtdyGqw&usqp=CAE",
+];
+const hoursMinSecs = { hours: 2, minutes: 20, seconds: 40 };
 
 let prodList = [];
 let counter = 1;
 for (let i of products) {
   prodList.push(
     <div class="gallery">
-      <a target="_blank" href="">
+      <a target="" href="#">
         <img src={i} alt={counter} width="600" height="600"></img>
       </a>
     </div>
   );
   counter++;
-} 
+}
 
 function App() {
   return (
@@ -33,8 +43,20 @@ function App() {
           alt="3"
         />
       </Carousel>
-      <p class="h1App" >FLASH DEALS! <Timer hoursMinSecs={hoursMinSecs}/></p>
-      {prodList}
+      <p class="h1App">
+        FLASH DEALS! <Timer hoursMinSecs={hoursMinSecs} />
+      </p>
+      <div>{prodList}</div>
+      <div id="banner">
+        <input type="checkbox" id="hideCheckbox"></input>
+        <label for="hideCheckbox">
+          <img
+            src="https://cf.shopee.ph/file/ph-50009109-edbd8b5f9f4d2c70f06b4f06dd7beba7"
+            alt="banner"
+            class="hideMe"
+          ></img>
+        </label>
+      </div>
     </div>
   );
 }
