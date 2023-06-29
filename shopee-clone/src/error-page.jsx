01b94a5d5ba1d04aa7170c62a25d84d8da66fac3
John Kyle Joseph Reyes <jkj313.kr@gmail.com>
2023-06-29 17:25:00 +0800

    update error page design

--- shopee-clone/src/error-page.jsx
+++ shopee-clone/src/error-page.jsx
@@ -6,11 +6,21 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <section class="bg-orange dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+          <div class="mx-auto max-w-screen-sm text-center">
+            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-white dark:text-primary-500">
+              404
+            </h1>
+            <p class="mb-4 text-3xl tracking-tight font-bold text-white md:text-4xl dark:text-white">
+              Something's missing.
+            </p>
+            <p class="mb-4 text-lg font-light text-white dark:text-gray-400">
+              Sorry, we can't find that page.{" "}
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
